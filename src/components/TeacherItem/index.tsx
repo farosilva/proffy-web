4bb4f208f2a3aea44d463bcd0ff90477abef51dd
Fabricio Silva <fabricio.roddrigo@gmail.com>
2020-08-06 08:48:26 -0300

    Connecting API Proffy

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: Number;
+  avatar: string;
+  bio: string;
+  cost: Number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/57637983?s=460&u=1eeefc4ba446d134ab0fbaeb256e5b864eb8a206&v=4" alt="Fabricio Silva"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Fabricio Silva</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br/> <br/>
-        Apaixonado por explodir coisas em laboratórios e por mudar a vida das pesosas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
       
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>{teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
